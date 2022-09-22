d5cc9224726cc036142f61af1c88e0446559256d
agusttin1 garcia <garciaagusttin@gmail.com>
2022-09-22 19:03:27 -0300

    cambios carrito. JSON y localStorage

--- js/conciertos.js
+++ js/conciertos.js
@@ -1,180 +1,206 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////Inyeccion HTML desde js para mostrar las entradas disponibles Y evento en el boton de su card///////////////////////////////
 
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-              ///////////////////////////////Inyeccion HTML desde js para mostrar las entradas disponibles Y evento en el boton de su card///////////////////////////////
+let carrito = [];
+const callBtnVaciar = document.getElementById(`btn-vaciar-modal`)
 
-let carrito = []
-
-
-function entradasMostrar (){
-
-    const callElementEntradas = document.getElementById(`contenedor-productos`)
+function entradasMostrar() {
+const callElementEntradas = document.getElementById(`contenedor-productos`);
 
 for (const datosEntrada of allEntradas) {
-    let columnsEntradas = document.createElement(`div`)
-    columnsEntradas.id += `entrada-${datosEntrada.id}` 
-    columnsEntradas.className= `${datosEntrada.categoria}  hide card-item ${datosEntrada.pais}` 
+    let columnsEntradas = document.createElement(`div`);
+    columnsEntradas.id += `entrada-${datosEntrada.id}`;
+    columnsEntradas.className = `${datosEntrada.categoria}  hide card-item ${datosEntrada.pais}`;
     columnsEntradas.innerHTML = `
-    <div class="card"  style="width: 18rem;">
-    <img src=${datosEntrada.img} class="card-img-top" alt="...">
-    <div class="card-body">
-    <p>
-        <span>${datosEntrada.fecha}</span>
-        <span>· ${datosEntrada.ciudad}</span>
-    </p>
-    <p class="card-text">${datosEntrada.descripEntrada} </p>
-    <button id="btn-comprar-${datosEntrada.id}" class="btn btn-primary">Agregar tickets</button>
-    </div>
-    <span class="price-event">$ ${datosEntrada.precio}</span>
-    <div class="pais-artist">
-    <img src=${datosEntrada.paisArtistaImg} alt="">
-    </div>
-</div>`
+                <div class="card"  style="width: 18rem;">
+                <img src=${datosEntrada.img} class="card-img-top" alt="...">
+                <div class="card-body">
+                <p>
+                    <span>${datosEntrada.fecha}</span>
+                    <span>· ${datosEntrada.ciudad}</span>
+                </p>
+                <p class="card-text">${datosEntrada.descripEntrada} </p>
+                <button id="btn-comprar-${datosEntrada.id}" class="btn btn-primary">Agregar tickets</button>
+                </div>
+                <span class="price-event">$ ${datosEntrada.precio}</span>
+                <div class="pais-artist">
+                <img src=${datosEntrada.paisArtistaImg} alt="">
+                </div>
+            </div>`;
 
-    callElementEntradas.appendChild(columnsEntradas)
+    callElementEntradas.appendChild(columnsEntradas);
 
+    let callBtnCompra = document.getElementById(
+    `btn-comprar-${datosEntrada.id}`
+    );
 
+    callBtnCompra.addEventListener(`click`, () => {
+    carrito.push({
+        id: datosEntrada.id,
+        img: datosEntrada.img,
+        descripEntrada: datosEntrada.descripEntrada,
+        precio: datosEntrada.precio,
+        cantidad:datosEntrada.cantidad
+    });
+      //console.log(carrito)
+    const callContadorCarrito = document.getElementById(`contador-carrito`);
+    callContadorCarrito.innerText = carrito.length;
 
+actualizarEntradaStorage()
 
-    let callBtnCompra = document.getElementById(`btn-comprar-${datosEntrada.id}`)
-
-    callBtnCompra.addEventListener(`click`,()=>{
-        carrito.push({
-            id:datosEntrada.id,
-            img:datosEntrada.img,
-            descripEntrada:datosEntrada.descripEntrada,
-            precio:datosEntrada.precio
-
-        })
-        //console.log(carrito)
-        const callContadorCarrito = document.getElementById(`contador-carrito`)
-        callContadorCarrito.innerText=carrito.length
-    })
- 
+    });
 }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-              ///////////////////////////////Funcion para filtrar las entradas por pais y categoria. Usado con un onclick dentro del html///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////Filtrado de las entradas, en base a su categoria y pais del artista. funciones aplicadas en onclicks dentro del html ///////////////////////////////
 
-function filtrosEntradas(value){
-    let callButtonsCategoria = document.querySelectorAll(".button-value")
-  
-    callButtonsCategoria.forEach((callButtonsARecorrer)=>{
-        if(value == callButtonsARecorrer.innerText){
-            callButtonsARecorrer.classList.add("active")
-        }else{
-            callButtonsARecorrer.classList.remove("active")
-        }
-    })
-        
-    let elements = document.querySelectorAll(".card-item")
-    elements.forEach((elemento)=>{
-        if(value == "All"){
-            elemento.classList.remove("hide")
-        }else{
-            if(elemento.classList.contains(value)){
-                elemento.classList.remove('hide')
-            }
-            else{
-                elemento.classList.add("hide")
-            }
-        }
-    })
+function filtrosEntradas(value) {
+let callButtonsCategoria = document.querySelectorAll(".button-value");
+
+callButtonsCategoria.forEach((callButtonsARecorrer) => {
+    if (value == callButtonsARecorrer.innerText) {
+    callButtonsARecorrer.classList.add("active");
+    } else {
+    callButtonsARecorrer.classList.remove("active");
     }
+});
+
+let elements = document.querySelectorAll(".card-item");
+elements.forEach((elemento) => {
+    if (value == "All") {
+    elemento.classList.remove("hide");
+    } else {
+    if (elemento.classList.contains(value)) {
+        elemento.classList.remove("hide");
+    } else {
+        elemento.classList.add("hide");
+    }
+    }
+});
+}
+
+//funcion anonima para que cargue automaticamente los productos a partir de los filtros
+window.onload = () => {
+filtrosEntradas("All");
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////Carrito funcional mediante un onclick incorporado en el html , onclicks en el footer del modal y en el el body modal dentro de las cards///////////////////////////////
+
 
    
-
-//funcion anonima para que cargue automaticamente los productos
-window.onload = () =>{
-    filtrosEntradas('All')
-  
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-              ///////////////////////////////Inyeccion HTML desde js para mostrar las entradas disponibles Y evento en el boton de su card///////////////////////////////
-
-
-
-function carritoFuncional (){
-const callCarrito = document.getElementById(`carrito`)
-const callContModalCarrito = document.getElementById(`cont-modal-eventos`)
-
-
-callCarrito.addEventListener(`click`,()=>{
-
-    callContModalCarrito.innerHTML=``
-
-    callContModalCarrito.style.display=`block`
-
-    const headerModal = document.createElement(`div`)
-    headerModal.innerHTML=`
-    <div class="cont-header-modal d-flex justify-content-between align-items-center">
-    <h1 class="title-modal">Bienvenido al Carrito</h1>
-    <span class="boton-salir-modal mr-2" id="boton-salir-x">X</span>
-    </div>
-    `
-    callContModalCarrito.appendChild(headerModal)
-
-    const callXBotonSalir = document.getElementById(`boton-salir-x`)
-    callXBotonSalir.addEventListener(`click`, ()=>{
-        callContModalCarrito.style.display=`none`
-        
-    })
-
-
-    for(let dataOfCarrito of carrito){
-        let contInCarrito = document.createElement(`div`)
-        contInCarrito.innerHTML = `
-        <div class="body-modal">
-        <div class="cont-img">
-        <img src=${dataOfCarrito.img} alt="">
-        </div>
-        <div class="body-info-modal">
-        <p class="card-text">${dataOfCarrito.descripEntrada}</p>
-        </div>
-        <span class="price-event-modal">$ ${dataOfCarrito.precio}</span>
     
-        </div>
-        `
 
-        callContModalCarrito.appendChild(contInCarrito)
+function carritoFuncional() {
+    const callContModalCarrito = document.getElementById(`cont-modal-eventos`);
 
-    }
-    
-    const accTotalAPagar = carrito.reduce((acc,el) => acc + el.precio,0)
 
-    const totalForBuy = document.createElement(`div`)
-    totalForBuy.innerHTML=`
-    <div class="footer-modal">
-    <div class="cont-total-modal">
-    <p>el total a pagar es: $${accTotalAPagar.toFixed(2)}</p>
-    </div>
-    <div class="cont-botones-modal">
-    <button id="btn-volver-modal" class="btn ">Volver</button>
-    <button id="btn-pagar-modal" class="btn ">Ir a pagar</button>
-    </div>
-    </div>
-    `
-    callContModalCarrito.appendChild(totalForBuy)
+            callContModalCarrito.innerHTML = ``;
 
-    const callBtnVolver = document.getElementById(`btn-volver-modal`)
-    callBtnVolver.addEventListener(`click`, ()=>{
-        callContModalCarrito.style.display="none"
+            callContModalCarrito.style.display = `block`;
         
-    })
+            const headerModal = document.createElement(`div`);
+            headerModal.innerHTML = `
+                        <div class="cont-header-modal d-flex justify-content-between align-items-center">
+                        <h1 class="title-modal">Bienvenido al Carrito</h1>
+                        <span class="boton-salir-modal mr-2" id="boton-salir-x">X</span>
+                    </div>
+                        `;
+            callContModalCarrito.appendChild(headerModal);
+        
+            const callXBotonSalir = document.getElementById(`boton-salir-x`);
+            callXBotonSalir.addEventListener(`click`, () => {
+            callContModalCarrito.style.display = `none`;
+            });
+        
+            for (let dataOfCarrito of carrito) {
+            let contInCarrito = document.createElement(`div`);
+            contInCarrito.innerHTML = `
+                            <div class="body-modal">
+                            <div class="cont-img">
+                            <img src=${dataOfCarrito.img} alt="">
+                            </div>
+                            <div class="body-info-modal">
+                            <p class="card-text">${dataOfCarrito.descripEntrada}</p>
+                        
+                            </div>
+                            <span class="price-event-modal">$ ${dataOfCarrito.precio}</span>
+                            <button class="btn-eliminar-entrada" onclick="eliminarItem(${dataOfCarrito.id})"><i class="fas fa-trash-alt"></i></button>
+                        
+                            </div>
+                            `;
+        
+            callContModalCarrito.appendChild(contInCarrito);
+            }
+            
+            const callContadorCarrito = document.getElementById(`contador-carrito`);
+            callContadorCarrito.innerText = carrito.length;
+        
+            const accTotalAPagar = carrito.reduce((acc, el) => acc + el.precio , 0);
+        
+            const totalForBuy = document.createElement(`div`);
+            totalForBuy.innerHTML = `
+                        <div class="footer-modal">
+                        <div class="cont-total-modal">
+                        <p>el total a pagar es: $${accTotalAPagar.toFixed(2)}</p>
+                        </div>
+                        <div class="cont-botones-modal mt-2">
+                        <button id="btn-volver-modal" class="btn ">Volver</button>
+                        <button id="btn-pagar-modal" class="btn "><a href="../page/finalizarCompra.html">Finalizar Compra</a></button>
+                        <button id="btn-vaciar-modal" onclick="eliminarEntradaCarrito()"class="btn ">vaciar carrito</button>
+                        </div>
+                        </div>
+                        `;
+            callContModalCarrito.appendChild(totalForBuy);
+        
+            const callBtnVolver = document.getElementById(`btn-volver-modal`);
+            callBtnVolver.addEventListener(`click`, () => {
+            callContModalCarrito.style.display = "none";
+            });
 
 
-})
 
 }
 
-function main(){
-    entradasMostrar()
+
+
+function eliminarEntradaCarrito(){
+carrito.length=0
+carritoFuncional()
+actualizarEntradaStorage()
+} 
+
+const eliminarItem = (id)=>{
+    let borrar = carrito.find((producto) => producto.id ===id)
+    let inidce = carrito.indexOf(borrar)
+    carrito.splice(inidce,1)
     carritoFuncional()
+    actualizarEntradaStorage()
 }
 
-main()
+function actualizarEntradaStorage() {
+    let productosJSON = JSON.stringify(carrito);
+    localStorage.setItem("productos", productosJSON);
+}
+
+function obtenerEntradaStorage() {
+    let productosJSON = localStorage.getItem("productos");
+    if (productosJSON) {
+    carrito = JSON.parse(productosJSON);
+    carritoFuncional();
+    }
+}
+
+
+function main() {
+entradasMostrar();
+obtenerEntradaStorage()
+
+}
+
+main();
--- js/stockEntradas.js
+++ js/stockEntradas.js
@@ -3,7 +3,7 @@
                     ///////////////////////////////Funcion constructora Y Arreglos para las cards de las entradas///////////////////////////////
 
                     class entradas{
-                        constructor(id,descripEntrada,precio,categoria,pais,fecha,ciudad,img,paisArtistaImg){
+                        constructor(id,descripEntrada,precio,categoria,pais,fecha,ciudad,img,paisArtistaImg,cantidad){
                             this.id=id
                             this.descripEntrada=descripEntrada
                             this.precio=precio
@@ -13,22 +13,23 @@
                             this.ciudad = ciudad
                             this.img=img
                             this.paisArtistaImg=paisArtistaImg
+                            this.cantidad=cantidad
                         }
                     }
-                    let entradaUno = new entradas ( 1,`Concierto de Puma Blue en Los Ángeles`, 3447.04, `Alternativa/Independiente`,`Ingles`,`01 oct. 2022`,` Los Ángeles`,`../images/img-artista-inicio/puma-blue.jpg`, `../images/bandera-pais-artista/UK.jpg` )
-                    let entradaDos = new entradas ( 2,`Concierto de Puma Blue + Arlo Parks`, 7445.60, `Alternativa/Independiente`,`Ingles`, `06 oct. 2022`, ` Dallas`,`../images/img-artista-inicio/puma-blue.jpg`, `../images/bandera-pais-artista/UK.jpg`)
-                    let entradaTres = new entradas ( 3,`Concierto de 50 Cent en Icónica Sevilla Fest 2022`, 9358.17, `Hip-Hop/Rap`,`EEUU`, `22 sep. 2022 `,` Sevilla`,`../images/img-artista-inicio/50-cent.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
-                    let entradaCuatro = new entradas(4,`Concierto de 50 Cent en la Icónica Viena`,11472.88,`Hip-Hop/Rap`,`EEUU`, `28 sep. 2022`,` Viena`,`../images/img-artista-inicio/50-cent.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
-                    let entradaCinco = new entradas ( 5,`Concierto de Eminem en Amsterdam`, 10000.43 , `Hip-Hop/Rap`,`EEUU`,`01 sept. 2022`,` Amsterdam`,`../images/img-artista-inicio/eminem.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
-                    let entradaSeis = new entradas(6,`Concierto de Eminem en Icónica Sevilla Fest 2022`,9600.43,`Hip-Hop/Rap`,`EEUU`,`24 sept. 2022`,` Sevilla`, `../images/img-artista-inicio/eminem.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
-                    let entradaSiete = new entradas ( 7,`Concierto Imperdible de Snoop Dogg en Helsinki`, 17200.71, `Hip-Hop/Rap`,`EEUU`,`05 sep. 2022`,` Helsinki`, `../images/img-artista-inicio/snoop-doggy.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
-                    let entradaOcho  = new entradas(8,`Concierto Imperdible de Snoop Dogg en Oberhausen`,9260.86,`Hip-Hop/Rap`,`EEUU`,`13 sep. 2022`,` Oberhausen`, `../images/img-artista-inicio/snoop-doggy.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
-                    let entradaNueve = new entradas (9, `Concierto de Mala Junta Malandro`, 1800.43, `Trap`,`Argentino`,`15 oct. 2022`,` Capital Federal`,`../images/img-artista-inicio/malandro.jpg`, `../images/bandera-pais-artista/ARG.jpg`)
-                    let entradaDiez = new entradas (10, `Concierto de Mala Junta Malandro`, 2000.43, `Trap`,`Argentino`,`24 oct. 2022`,` Capital Federal`,`../images/img-artista-inicio/malandro.jpg`, `../images/bandera-pais-artista/ARG.jpg`)
-                    let entradaOnce = new entradas ( 11,`Concierto de Puma Blue + Arlo Parks`, 6480.43, `Alternativa/Independiente`,`Ingles`,`08 oct. 2022`,` Houston`,`../images/img-artista-inicio/puma-blue.jpg`, `../images/bandera-pais-artista/UK.jpg`)
-                    let entradaDoce = new entradas ( 12,`Concierto de 50 Cent en la Icónica Rotterdam`, 10526.84, `Hip-Hop/Rap`,`EEUU`,`22 oct. 2022`,` Rotterdam`, `../images/img-artista-inicio/50-cent.jpg`, `../images/bandera-pais-artista/EEUU.jpg`)
+                    let entradaUno = new entradas ( 1,`Concierto de Puma Blue en Los Ángeles`, 3447.04, `Alternativa/Independiente`,`Ingles`,`01 oct. 2022`,` Los Ángeles`,`../images/img-artista-inicio/puma-blue.jpg`, `../images/bandera-pais-artista/UK.jpg`,1 )
+                    let entradaDos = new entradas ( 2,`Concierto de Puma Blue + Arlo Parks`, 7445.60, `Alternativa/Independiente`,`Ingles`, `06 oct. 2022`, ` Dallas`,`../images/img-artista-inicio/puma-blue.jpg`, `../images/bandera-pais-artista/UK.jpg`,1)
+                    let entradaTres = new entradas ( 3,`Concierto de 50 Cent en Icónica Sevilla Fest 2022`, 9358.17, `Hip-Hop/Rap`,`EEUU`, `22 sep. 2022 `,` Sevilla`,`../images/img-artista-inicio/50-cent.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaCuatro = new entradas(4,`Concierto de 50 Cent en la Icónica Viena`,11472.88,`Hip-Hop/Rap`,`EEUU`, `28 sep. 2022`,` Viena`,`../images/img-artista-inicio/50-cent.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaCinco = new entradas ( 5,`Concierto de Eminem en Amsterdam`, 10000.43 , `Hip-Hop/Rap`,`EEUU`,`01 sept. 2022`,` Amsterdam`,`../images/img-artista-inicio/eminem.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaSeis = new entradas(6,`Concierto de Eminem en Icónica Sevilla Fest 2022`,9600.43,`Hip-Hop/Rap`,`EEUU`,`24 sept. 2022`,` Sevilla`, `../images/img-artista-inicio/eminem.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaSiete = new entradas ( 7,`Concierto Imperdible de Snoop Dogg en Helsinki`, 17200.71, `Hip-Hop/Rap`,`EEUU`,`05 sep. 2022`,` Helsinki`, `../images/img-artista-inicio/snoop-doggy.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaOcho  = new entradas(8,`Concierto Imperdible de Snoop Dogg en Oberhausen`,9260.86,`Hip-Hop/Rap`,`EEUU`,`13 sep. 2022`,` Oberhausen`, `../images/img-artista-inicio/snoop-doggy.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaNueve = new entradas (9, `Concierto de Mala Junta Malandro`, 1800.43, `Trap`,`Argentino`,`15 oct. 2022`,` Capital Federal`,`../images/img-artista-inicio/malandro.jpg`, `../images/bandera-pais-artista/ARG.jpg`,1)
+                    let entradaDiez = new entradas (10, `Concierto de Mala Junta Malandro`, 2000.43, `Trap`,`Argentino`,`24 oct. 2022`,` Capital Federal`,`../images/img-artista-inicio/malandro.jpg`, `../images/bandera-pais-artista/ARG.jpg`,1)
+                    let entradaOnce = new entradas ( 11,`Concierto Lynyrd Skynyrd en Las Vegas Parkin Lots`, 12480.43, `Rock`,`EEUU`,`09 dec. 2022`,` Nevada`,`../images/img-artista-inicio/lynyrdskynyrd.jpg`, `../images/bandera-pais-artista/EEUU.jpg`,1)
+                    let entradaDoce = new entradas ( 12,`Concierto de Andres Calamaro el arena Peru`, 9526.84, `Rock`,`Argentino`,`15 oct. 2022`,` Lima Peru`, `../images/img-artista-inicio/calamaro.jpg`, `../images/bandera-pais-artista/ARG.jpg`,1)
                     
                     const allEntradas = [entradaUno,entradaDos,entradaTres,entradaCuatro,entradaCinco,entradaSeis,entradaSiete,entradaOcho,entradaNueve,entradaDiez ,entradaOnce,entradaDoce]
                     
 
-                 
+    
