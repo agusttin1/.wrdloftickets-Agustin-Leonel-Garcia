a5bfd4349ea8e097e6327421d55af6a58f2c1c9c
agusttin1 garcia <garciaagusttin@gmail.com>
2022-10-04 22:02:33 -0300

    scipt ordenados

---
+++ js/inicio.js
@@ -0,0 +1,23 @@
+function clickInTheOptionLogOut (){
+    Swal.fire({
+      title: 'Estas seguro que desea cerrar sesion?',
+      text: `PERDERA,las entradas que selecciono`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Cerrar Sesion de todas formas!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+      
+          location.href="../index.html"
+        localStorage.removeItem(`key-entrada`)
+        carrito=[]
+         
+        
+        
+      }
+    })
+   
+  
+  }
--- js/regAndLogin.js
+++ js/regAndLogin.js
@@ -198,29 +198,6 @@ function ObtenerUsuariosStorage(){
 
 // fnction a ejecutar en inicio.html
 
-function clickInTheOptionLogOut (){
-  Swal.fire({
-    title: 'Estas seguro que desea cerrar sesion?',
-    text: `PERDERA,las entradas que selecciono`,
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Cerrar Sesion de todas formas!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-    
-        location.href="../index.html"
-      localStorage.removeItem(`key-entrada`)
-      carrito=[]
-       
-      
-      
-    }
-  })
- 
-
-}
 
 
 function mainLogReg(){
